03bc1485dc9f43471b357dddc78ee91d3a3c9124
thotluna <eladi.feijoo@gmail.com>
2022-06-13 23:07:59 -0400

    Load and setting of font family

--- app/src/main/java/ve/com/teeac/mynewapplication/ui/theme/Type.kt
+++ app/src/main/java/ve/com/teeac/mynewapplication/ui/theme/Type.kt
@@ -1,10 +1,23 @@
 package ve.com.teeac.mynewapplication.ui.theme
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import ve.com.teeac.mynewapplication.R
+
+val robotoCondensedLight = FontFamily(
+    Font( R.font.roboto_condensed_light ),
+)
+val robotoCondensedBold = FontFamily(
+    Font( R.font.roboto_condensed_bold ),
+)
+val robotoCondensedRegular = FontFamily(
+    Font( R.font.roboto_condensed_regular ),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,7 +27,17 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = robotoCondensedRegular,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = robotoCondensedBold,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
