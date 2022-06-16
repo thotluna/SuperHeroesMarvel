673bedb2844cbebe25f06f7f4be0fee4200c304b
thotluna <eladi.feijoo@gmail.com>
2022-06-16 19:25:56 -0400

    Create navigate to Character Detail

--- app/src/main/java/ve/com/teeac/mynewapplication/presentations/navigations/DestinationScreen.kt
+++ app/src/main/java/ve/com/teeac/mynewapplication/presentations/navigations/DestinationScreen.kt
@@ -1,16 +1,12 @@
 package ve.com.teeac.mynewapplication.presentations.navigations
 
 sealed class DestinationScreen(val route: String){
-    object Characters : DestinationScreen("characters_screen")
-    object CharacterDetail : DestinationScreen("character_detail_screen")
 
-    fun withArgs(vararg args: String): String {
-        return buildString {
-            append(route)
-            args.forEach { arg ->
-                append("/")
-                append(arg)
-            }
+    object Characters : DestinationScreen("characters_screen")
+
+    object CharacterDetail : DestinationScreen("character_detail_screen"){
+        fun createRoute(id: Int = -1): String {
+            return route +"?id=${id}"
         }
     }
 }
